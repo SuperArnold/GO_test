64dd46a41aa41eaa76e46897732396f6c79ed9c5
SuperArnold <ji394vul35520@hotmail.com>
2021-03-20 01:56:01 +0800

    第五種宣告方式

--- 1.GO宣吿變數/main.go
+++ 1.GO宣吿變數/main.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+const (
+	monday = 1
+	tusday = 2
+)
+
 func main() {
 	foo := "Arnold"
 	bar := 100
 	fmt.Println(foo)
 	fmt.Println(bar)
+	fmt.Println(monday)
+	fmt.Println(tusday)
 	fmt.Println("Done")
 }
