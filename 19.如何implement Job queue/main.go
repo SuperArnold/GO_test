c3ce76e303f8333ecf2ce857033c68b5aea9d006
SuperArnold <ji394vul35520@hotmail.com>
2021-03-21 02:22:25 +0800

    測試blocking的話，把1024改小

--- 19.如何implement Job queue/main.go
+++ 19.如何implement Job queue/main.go
@@ -8,14 +8,20 @@ import (
 func worker(jobchan_work <-chan int) {
 	for i := range jobchan_work {
 		fmt.Printf("Worker is %d\n", i)
+		time.Sleep(1 * time.Second)
 	}
 }
+
 func main() {
-	jobchan := make(chan int, 1024)
+	jobchan := make(chan int, 1)
+
 	go worker(jobchan)
 	jobchan <- 1
 	fmt.Println("main is 1")
 	jobchan <- 2
 	fmt.Println("main is 2")
+	jobchan <- 3
+	fmt.Println("main is 3")
+
 	time.Sleep(2 * time.Second)
 }
