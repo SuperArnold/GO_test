5dcc6f638785fd851459b34dad9aabc284618397
SuperArnold <ji394vul35520@hotmail.com>
2021-03-21 01:09:58 +0800

    另一種寫法

--- 2.func用法/main.go
+++ 2.func用法/main.go
@@ -2,13 +2,10 @@ package main
 
 import "fmt"
 
-func foo() func() int { //回傳func
-	return func() int {
-		return 100
-	}
-}
 func main() {
-	bar := foo()
-	fmt.Printf("%T\n", bar) //印出func定義
-	fmt.Println(bar())
+	bar := func(i, j int) int { //執行將func寫進去
+		return i + j
+	}
+	fmt.Printf("%T\n", bar)
+	fmt.Println(bar(1, 5))
 }
