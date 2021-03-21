3669e08cd64cf532edda8d9ca2811960f6b98e08
SuperArnold <ji394vul35520@hotmail.com>
2021-03-21 23:14:36 +0800

    因傳址過去，但是不是改到原本的，所以外面的不會有影響

--- 04. Slice做function的參數技巧/main.go
+++ 04. Slice做function的參數技巧/main.go
@@ -4,13 +4,13 @@ import (
 	"fmt"
 )
 
-func modify(foo []string) {
-	foo[1] = "c"
+func add(foo []string) {
+	foo = append(foo, "c")
 	fmt.Println("Modify:", foo)
 }
 func main() {
 	foo := []string{"a", "b"}
 	fmt.Println("before foo :", foo)
-	modify(foo)
+	add(foo)
 	fmt.Println("after foo :", foo)
 }
