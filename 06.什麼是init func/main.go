e4013d65879dfa168cba578c27c2e891d2bc9fda
SuperArnold <ji394vul35520@hotmail.com>
2021-03-23 00:37:39 +0800

    將init放在最前面，卻還是可以看到還是為0，原因是因為不管有做哪些事情，init只會執行在main的前面

--- 06.什麼是init func/main.go
+++ 06.什麼是init func/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 )
 
+func init() {
+	global = 0
+}
+
 var global = convert()
 
 func convert() int {
 	return 100
-}
-func init() {
-	global = 0
 }
 func main() {
 	fmt.Println("Global is ", global)
