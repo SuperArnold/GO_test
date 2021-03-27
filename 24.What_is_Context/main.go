453a8efed3510ab2dfa41709af3608d69382e989
SuperArnold <ji394vul35520@hotmail.com>
2021-03-28 02:55:39 +0800

    實作多個go routine同時被取消的功能

--- 24.What_is_Context/main.go
+++ 24.What_is_Context/main.go
@@ -9,20 +9,25 @@ import (
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
-	go func() {
-		for {
-			select {
-			case <-ctx.Done():
-				fmt.Println("stop job.")
-				return
-			default:
-				fmt.Println("still work")
-				time.Sleep(1 * time.Second)
-			}
-		}
-	}()
+	go work(ctx, "node 1")
+	go work(ctx, "node 2")
+	go work(ctx, "node 3")
+
 	time.Sleep(5 * time.Second)
 	cancel()
 	time.Sleep(1 * time.Second)
 	fmt.Println("All Job Done.")
+}
+
+func work(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println(name + " stop job.")
+			return
+		default:
+			fmt.Println(name + " still work")
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
