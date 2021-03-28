08c91d97bae36251eacde8272e9018078106faf5
SuperArnold <ji394vul35520@hotmail.com>
2021-03-29 01:14:24 +0800

    避免會有遺漏的問題，就會將select拆開來寫，也避免多執行一次的相關地雷

--- 25.Select_Random_Channel/main.go
+++ 25.Select_Random_Channel/main.go
@@ -20,8 +20,12 @@ func main() {
 	for i := 0; i < 30; i++ {
 		select {
 		case channel <- i:
+		}
+
+		select {
 		case <-tick.C:
 			fmt.Printf("%d is tick\n", i)
+		default: //避免卡在tick中
 		}
 
 		time.Sleep(200 * time.Millisecond)
