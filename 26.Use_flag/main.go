074f2b7dc6ca38eeb2b9741fa52cf18f2f02623e
SuperArnold <ji394vul35520@hotmail.com>
2021-03-30 01:29:07 +0800

    加上監控ping的方式，確認Server是否活著

--- 26.Use_flag/main.go
+++ 26.Use_flag/main.go
@@ -12,14 +12,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "I try to use %s\n", r.URL.Path[1:])
 }
 
+func pinger(port string) error {
+	resp, err := http.Get("http://localhost:" + port)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		fmt.Errorf("server's status is not 200")
+	}
+	return nil
+
+}
+
 func main() {
 	var port string
+	var ping bool
 	flag.StringVar(&port, "port", "8080", "server port")
 	flag.StringVar(&port, "p", "8080", "server port")
+	flag.BoolVar(&ping, "ping", false, "ping server is live")
 	flag.Parse()
 
 	if p, ok := os.LookupEnv("PORT"); ok {
 		port = p
+	}
+
+	if ping {
+		if err := pinger(port); err != nil {
+			log.Printf("Server had some error")
+			return
+		}
+		log.Printf("Server is live")
+		return
 	}
 
 	http.HandleFunc("/", handler)
