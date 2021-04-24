9e95c6bb6585436433edf774a11b749b882b0853
SuperArnold <ji394vul35520@hotmail.com>
2021-04-25 03:12:51 +0800

    解決寄錯人的問題，方法一：產生10個struct避免互相影響

--- 07.struct_methon_points_vs_value/main.go
+++ 07.struct_methon_points_vs_value/main.go
@@ -20,9 +20,9 @@ func (e *email) Send() {
 	fmt.Printf("Send emaol From %s To %s\n", e.from, e.to)
 }
 func main() {
-	email := &email{}
 	for i := 1; i <= 10; i++ {
 		go func(i int) {
+			email := &email{} //移到這裡
 			email.From(fmt.Sprintf("User%02d", i))
 			email.To(fmt.Sprintf("User%02d", i+1))
 			email.Send()
