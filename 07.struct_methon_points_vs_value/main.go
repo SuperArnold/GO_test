472b6aba2cbc8b372db79abaa8aebce38bccb82f
SuperArnold <ji394vul35520@hotmail.com>
2021-04-25 03:10:00 +0800

    當setCarName01跟setCarName02互換，結果都是bar

--- 07.struct_methon_points_vs_value/main.go
+++ 07.struct_methon_points_vs_value/main.go
@@ -8,16 +8,16 @@ type car struct {
 	name string
 }
 
-func (c car) setCarName01(s string) { //傳value
+func (c car) setCarName01(s string) { //這裡其實是有兩個struct，而改的是這個function內的struct
 	c.name = s
 }
-func (c *car) setCarName02(s string) { //傳pointer
+func (c *car) setCarName02(s string) {
 	c.name = s
 }
 func main() {
 	c := &car{}
-	c.setCarName01("foo")
-	fmt.Println(c.name)
 	c.setCarName02("bar")
+	fmt.Println(c.name)
+	c.setCarName01("foo")
 	fmt.Println(c.name)
 }
