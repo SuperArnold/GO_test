136faef5554b326b756555e3bc8fd9a523d178a5
SuperArnold <ji394vul35520@hotmail.com>
2021-04-25 03:13:26 +0800

    解決寄錯人的問題，方法二：把pointer拿掉，並回傳email的struct，這樣子就不會用到錯的value的問題

--- 07.struct_methon_points_vs_value/main.go
+++ 07.struct_methon_points_vs_value/main.go
@@ -10,22 +10,24 @@ type email struct {
 	to   string
 }
 
-func (e *email) From(s string) {
+func (e email) From(s string) email {
 	e.from = s
+	return e
 }
-func (e *email) To(s string) {
+func (e email) To(s string) email {
 	e.to = s
+	return e
 }
-func (e *email) Send() {
+func (e email) Send() {
 	fmt.Printf("Send emaol From %s To %s\n", e.from, e.to)
 }
 func main() {
+	email := &email{}
 	for i := 1; i <= 10; i++ {
 		go func(i int) {
-			email := &email{} //移到這裡
-			email.From(fmt.Sprintf("User%02d", i))
-			email.To(fmt.Sprintf("User%02d", i+1))
-			email.Send()
+			email.From(fmt.Sprintf("User%02d", i)).
+				To(fmt.Sprintf("User%02d", i+1)).
+				Send()
 		}(i)
 	}
 	time.Sleep(1 * time.Second)
